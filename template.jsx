8dad72e0e41c8bf2a9aadf6af54c1e6562484f9e
Diego Vazquez <diegovazquezny@gmail.com>
2020-12-26 19:23:53 -0500

    need to move inline styles to style sheet

--- template.jsx
+++ template.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { connect } from 'react-redux';
-//import * as actions from '../../actions/actions';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import * as actions from '../../actions/actions';
 
-const mapDispatchToProps = dispatch => ({
- // updateUserInfo: (data) => dispatch(actions.updateUserInfo(data)),
-});
+// const mapDispatchToProps = dispatch => ({
+//  func: (data) => dispatch(actions.func(data)),
+// });
 
-const mapStateToProps = ({
-  reducer: { userName, picture, authenticated }
-}) => ({ userName, picture, authenticated });
+// const mapStateToProps = ({
+//   reducer: { }
+// }) => ({ });
 
-const GameCard = (props) => {
-  return (
-    <div>
-      hello
-    </div>
-  );
-}
+// const Name = (props) => {
+//   return (
+//     <div>
+//       hello
+//     </div>
+//   );
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameCard);
+// export default connect(mapStateToProps, mapDispatchToProps)(Name);
